c55141e95baa05265f660b05af2b8afe4a65d9a7
alihaidari079 <149894041+alihaidari079@users.noreply.github.com>
2025-08-03 18:53:58 +0430

    Add files via upload

---
+++ resturant/index.js
@@ -0,0 +1,6 @@
+// function getCursorPos(element,e) {
+//     const rect = element.getBoundingClientRect();
+//     return {
+//         x: e.clientC - rect.left, y: e.clientY - rect.top
+//     };
+// }
